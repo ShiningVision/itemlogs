b22530c963e6195f126629a0e056dbc28758d1c8
ShiningVision <antonchen00@gmx.at>
2026-07-11 01:49:05 +0200

    Tutorial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <p>Welcome Home!</p>;
+  {/**Insert show items settings here
+    And Switch to show view button
+    And Theme slider */}
+}
---
+++ themes/themeRegistry.ts
@@ -0,0 +1,8 @@
+// themes/themeRegistry.ts
+import standard from './standard.module.css';
+import dark from './dark.module.css';
+
+export const themeRegistry = {
+  standard,
+  dark,
+} as const;
